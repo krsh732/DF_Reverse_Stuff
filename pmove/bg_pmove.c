6943304b78d2f33c786baba9ec86cb8f91c31daf
Krishna Ravishankar <krishna.ravi732@gmail.com>
2019-07-07 15:25:33 -0400

    Reverse PM_Friction

--- pmove/bg_pmove.c
+++ pmove/bg_pmove.c
@@ -89,3 +89,66 @@ void PM_ClipVelocity( vec3_t in, vec3_t normal, vec3_t out, float overbounce ) {
 	}
 }
 
+static void PM_Friction( void ) {
+	vec3_t	vec;
+	float	*vel;
+	float	speed, newspeed, control;
+	float	drop;
+
+	vel = pm->ps->velocity;
+
+	VectorCopy( vel, vec );
+	if ( pml.walking ) {
+		vec[2] = 0;	// ignore slope movement
+	}
+
+	speed = VectorLength(vec);
+	if (speed < 1) {
+		vel[0] = 0;
+		vel[1] = 0;		// allow sinking underwater
+		// FIXME: still have z friction underwater?
+		return;
+	}
+
+	drop = 0;
+
+	// apply ground friction
+	if ( pm->waterlevel <= 1 ) {
+		if ( pml.walking && !(pml.groundTrace.surfaceFlags & SURF_SLICK) ) {
+			// if getting knocked back, no friction
+			if ( ! (pm->ps->pm_flags & PMF_TIME_KNOCKBACK) ) {
+				control = speed < pm_stopspeed ? pm_stopspeed : speed;
+				drop += control*pm_friction*pml.frametime;
+			}
+		}
+	}
+
+	// apply water friction even if just wading
+	if ( pm->waterlevel ) {
+        if ( pm->ps->pm_flags & PMF_PROMODE ) {
+            drop += 0.5*speed*pm->waterlevel*pml.frametime;
+        } else {
+            drop += speed*pm_waterfriction*pm->waterlevel*pml.frametime;
+        }
+    }
+
+	// apply flying friction
+	if ( pm->ps->powerups[PW_FLIGHT]) {
+		drop += speed*pm_flightfriction*pml.frametime;
+	}
+
+	if ( pm->ps->pm_type == PM_SPECTATOR) {
+		drop += speed*pm_spectatorfriction*pml.frametime;
+	}
+
+	// scale the velocity
+	newspeed = speed - drop;
+	if (newspeed < 0) {
+		newspeed = 0;
+	}
+	newspeed /= speed;
+
+	vel[0] = vel[0] * newspeed;
+	vel[1] = vel[1] * newspeed;
+	vel[2] = vel[2] * newspeed;
+}
