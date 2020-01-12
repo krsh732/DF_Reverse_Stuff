eac11336a4f21cfbcf7a41fac4561987d64920b9
Krishna Ravishankar <krishna.ravi732@gmail.com>
2020-01-12 12:50:49 -0500

    Describe in more detail the timer event field

--- timer/timer.h
+++ timer/timer.h
@@ -1,7 +1,14 @@
+typedef enum {
+	TIMER_NO_EVENT,
+	TIMER_START_EVENT,
+	TIMER_STOP_EVENT,
+	TIMER_CHECKPOINT_EVENT
+} timerEvent_t;
+
 typedef struct {
-    int      time;
-    int      events; // verify and document more
+    int time;
+    timerEvent_t event;
     qboolean timer_running;
-    int      checkpoint_bitmap;
-    int      num_checkpoints;
-} timeInfo_t;
+    int checkpoint_bitmap;
+    int num_checkpoints;
+} timerInfo_t;
